bd9fcab743b8d1cef6a36ed2274230be03baf368
LidaKarpovich <94173269+LidaKarpovich@users.noreply.github.com>
2023-03-07 13:07:58 +0300

    Revert "Lab1"

--- Лабораторная 1/Скобочные группы/task.py
+++ Лабораторная 1/Скобочные группы/task.py
@@ -5,21 +5,7 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: Входная строка для проверки
     :return: True, если последовательность корректна, False в противном случае
     """
-    if len(brackets_row) % 2 != 0:
-        return False
-    para = {'(': ')'}
-    stack = []
-    for bracket in brackets_row:
-        if bracket in para.keys():
-            stack.append(bracket)
-        else:
-            if stack == []:
-                return False
-            open_bracket = stack.pop()
-            if bracket != para[open_bracket]:
-                return False
-
-    return stack == []
+    ...  # TODO реализовать проверку скобочной группы
 
 
 if __name__ == '__main__':
